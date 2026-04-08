29ec70d9b4b77298d275f1833c0a1a54021ec1dc
Edy <burdaedy@gmail.com>
2026-04-08 22:27:12 +0300

    poze vladut andrei darius nu erau afisate -fixed, EDY Please enter the commit message for your changes. Lines starting

--- js/config.js
+++ js/config.js
@@ -1,7 +1,7 @@
 const CS_TEAM = [
-    { name: "Vladut Andrei", role: "Project Manager", img: "assets/img/Vladut.jpeg", style: "", linkedin: "https://www.linkedin.com/in/vladut-nicolae-andrei/" },
-    { name: "Andrei Nitu", role: "Project Assistant", img: "assets/img/Andrei.jpeg", style: "", linkedin: "https://www.linkedin.com/in/nituandreiraul/" },
-    { name: "Darius Preda", role: "Project Assistant", img: "assets/img/Darius.jpeg", style: "", linkedin: "https://www.linkedin.com/in/darius-preda/" },
+    { name: "Vladut Andrei", role: "Project Manager", img: "assets/img/vladut.jpeg", style: "", linkedin: "https://www.linkedin.com/in/vladut-nicolae-andrei/" },
+    { name: "Andrei Nitu", role: "Project Assistant", img: "assets/img/andrei.jpeg", style: "", linkedin: "https://www.linkedin.com/in/nituandreiraul/" },
+    { name: "Darius Preda", role: "Project Assistant", img: "assets/img/darius.jpeg", style: "", linkedin: "https://www.linkedin.com/in/darius-preda/" },
     { name: "Eduard Moga", role: "BD Officer", img: "assets/img/edymoga.jpeg", style: "", linkedin: "https://www.linkedin.com/in/eduard-andrei-moga/" },
     { name: "Vlad Stingaciu", role: "BD Officer", img: "assets/img/vlad.jpeg", style: "", linkedin: "#" },
     { name: "Radu Lazar", role: "BD Officer", img: "assets/img/radu.jpeg", style: "object-position: top;", linkedin: "https://www.linkedin.com/in/radu-gabriel-laz%C4%83r-986034360/" },
